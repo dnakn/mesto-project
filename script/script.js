dad680a6bbac764abfe9b2d6598b07d6edfb682e
Diana Kondrateva <diana.tokareva.01@bk.ru>
2022-07-03 00:08:20 +0300

    Добавлена кнопка удаления, исправлена функция создания карточки

--- script/script.js
+++ script/script.js
@@ -48,60 +48,37 @@ const subTitle = document.querySelector(".profile__subtitle");
 
 const list = document.querySelector(".cards__list");
 
+// получаем template
+const cardTemplate = document.querySelector(".template").content;
+
 function createCard(item) {
-  // создаем li - корневой элемент карточки
-  const element = document.createElement("li");
-  // добавляем ему нужный класс
-  element.classList.add("card");
+  // клонируем элемент
+  const element = cardTemplate.querySelector(".card").cloneNode(true);
 
-  // создаем элемент img
-  const imageEl = document.createElement("img");
-  // добавляем ему класс для валидного отображения
-  imageEl.classList.add("card__image");
+  // получаем img
+  const image = element.querySelector(".card__image");
+  // получаем заголовок
+  const descriptionTitle = element.querySelector(".card__description-title");
+
   // тегу img добавляем значение элемента массива link в src
-  imageEl.src = item.link;
+  image.src = item.link;
   // тегу img добавляем значение элемента массива name в link
-  imageEl.alt = item.name;
+  image.alt = item.name;
 
-  // создаем div (контейнер) для отрисовки значений
-  const description = document.createElement("div");
-  // добавляем ему класс
-  description.classList.add("card__description");
-
-  // создаем заголовок карточки
-  const descriptionTitle = document.createElement("p");
-  // добавляем класс
-  descriptionTitle.classList.add("card__description-title");
   // вставляем текст в заголовок, текст из элемента массива
   descriptionTitle.textContent = item.name;
-
-  // создаем кнопку "нравится"
-  const likeButton = document.createElement("button");
-  // добавляем ей класс
-  likeButton.classList.add("card__like-button");
-
-  // ниже добавляем заголовок и кнопку нравится в контейнер описания
-  description.appendChild(descriptionTitle);
-  description.appendChild(likeButton);
-
-  // в корневой элемент карточки (li) добавляем изображение, которое мы создали
-  element.append(imageEl);
-  // так же добавляем desciption, appendChild добавляет в конец (описание должно быть под изображением)
-  element.appendChild(description);
 
   return element;
 }
 
 //функция создания карточек
-function createCards() {
+function renderCards() {
   // цикл для прохождения по элементам массива карточек
   initialCards.forEach((item) => {
     const element = createCard(item);
     list.appendChild(element);
   });
 }
-
-createCards();
 
 function closePopup(popupRef) {
   popupRef.classList.remove("popup_opened");
@@ -152,3 +129,5 @@ closeButton.addEventListener("click", () => closePopup(editPopup));
 closeButtonNewPlace.addEventListener("click", () => closePopup(addPopup));
 formElement.addEventListener("submit", formSubmitHandler);
 addForm.addEventListener("submit", addCard);
+
+renderCards();
